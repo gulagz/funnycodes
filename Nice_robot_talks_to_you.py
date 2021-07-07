0b197f666161a7ae569a3712fb364ac13d4d427d
gulagz <87048282+gulagz@users.noreply.github.com>
2021-07-06 21:51:05 -0300

    Update Nice_robot_talks_to_you.py

--- Nice_robot_talks_to_you.py
+++ Nice_robot_talks_to_you.py
@@ -4,9 +4,9 @@ import random
 compliments = ['Oh no! Remember you are valued!', 'It will get better', 'Sorry to hear! Try doing something that makes you happy, like eating ice-cream or watching netflix!']
 
 
-Hello = str(input('hello world, say hi back!'))
+Hello = str(input('hello world, say hi back!: '))
 
-How_are_you = str(input('Hello! how are you? (Good or Bad)'))
+How_are_you = str(input('Hello! how are you? (Good or Bad): '))
 
 if How_are_you == "Good":
     print('Good to hear!')
